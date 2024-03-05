5633c679e07f8593858fe872d8c63d0a4a4b0fe7
visciric <visciric@students.zhaw.ch>
2024-03-05 20:24:19 +0100

    changes?

--- venv/lib/python3.10/site-packages/modal_test_support/module_2.py
+++
@@ -1,3 +0,0 @@
-# Copyright Modal Labs 2022
-def square(x):
-    return x**2
--- venv/lib/python3.10/site-packages/modal_test_support/webhook_forgot_function.py
+++
@@ -1,7 +0,0 @@
-# Copyright Modal Labs 2023
-from modal import web_endpoint
-
-
-@web_endpoint()
-async def absent_minded_function(x):
-    pass

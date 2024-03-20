770113c57f4ea6553d88a4f698a5fdcd8cd9e130
visciric <visciric@students.zhaw.ch>
2024-03-20 20:28:26 +0100

    push

--- F1 Data Analysis/season_2024/settings.py
+++ F1 Data Analysis/season_2024/settings.py
@@ -1,1 +1,1 @@
-CACHE_FOLDER_PATH = r'C:\Users\ricca\OneDrive - ZHAW\Desktop\Projects\F1 Data Analysis\season_2024\cache'
+CACHE_FOLDER_PATH = r'C:\Users\ricca\OneDrive - ZHAW\Dokumente\f1_cache'
--- F1 Data Analysis/season_2024/utlis.py
+++ F1 Data Analysis/season_2024/utlis.py
@@ -1,11 +1,57 @@
 import os
 import fastf1 as ff1
+import plotly
+import plotly.graph_objs as go
 
 def setup_cache(path):
-    cache_folder = 'cache'
-    cache_folder_path = os.path.join(path, cache_folder)
+    if not os.path.exists(path):
+        os.makedirs(path)
 
-    if not os.path.exists(cache_folder_path):
-        os.makedirs(cache_folder_path)
+    ff1.Cache.enable_cache(path)
 
-    ff1.Cache.enable_cache(cache_folder_path)
+
+# Interactive plots 
+def plot_data_interactive(df_list, x_col, y_col, labels):
+    fig = go.Figure()
+
+    for i, df in enumerate(df_list):
+        fig.add_trace(go.Scatter(x=df[x_col], y=df[y_col], mode='lines', name=labels[i],
+                                 line=dict(color='blue' if i == 0 else 'red')))
+    
+    # Assuming circuit_info is available
+    v_min = min(df[y_col].min() for df in df_list)
+    v_max = max(df[y_col].max() for df in df_list)
+    
+    for _, corner in circuit_info.corners.iterrows():
+        txt = f"{corner['Number']}{corner['Letter']}"
+        fig.add_annotation(x=corner['Distance'], y=max(v_min-150, 0), text=txt,
+                           showarrow=False, font=dict(size=10))
+    
+    fig.update_layout(
+        xaxis=dict(title=x_col),
+        yaxis=dict(title=y_col),
+        title=f'Comparison of {y_col} between drivers',
+        legend=dict(title='Driver'),
+        hovermode='closest',
+        showlegend=True,
+        hoverlabel=dict(bgcolor="white", font_size=12, font_family="Rockwell")
+    )
+
+    fig.show()
+
+# example call 
+"""
+Before you call the function ensure to add distance and seconds 
+
+fastest_yuki = yuki.pick_fastest().get_car_data().add_distance()
+fastest_mag = mag.pick_fastest().get_car_data().add_distance()
+
+fastest_mag['sec'] = fastest_mag['Time'].dt.total_seconds()
+fastest_yuki['sec'] = fastest_yuki['Time'].dt.total_seconds()
+fastest_yuki
+
+
+plot_data_interactive([fastest_yuki, fastest_mag], 'Distance', 'RPM', ['Tsunoda', 'Magnussen'])
+
+
+"""

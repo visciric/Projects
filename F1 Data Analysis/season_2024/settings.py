4292af90df174b5a1b068a401b6cbb1f1f55457c
visciric <visciric@students.zhaw.ch>
2024-03-27 21:48:47 +0100

    changes

--- F1 Data Analysis/season_2024/settings.py
+++ F1 Data Analysis/season_2024/settings.py
@@ -1,1 +1,15 @@
-CACHE_FOLDER_PATH = r'C:\Users\ricca\OneDrive - ZHAW\Dokumente\f1_cache'
+# CACHE_FOLDER_PATH = r'C:\Users\ricca\OneDrive - ZHAW\Dokumente\f1_cache'
+
+
+import platform
+
+# Check the system platform
+if platform.system() == 'Windows':
+    CACHE_FOLDER_PATH = r'C:\Users\ricca\OneDrive - ZHAW\Dokumente\f1_cache'
+elif platform.system() == 'Darwin':  # 'Darwin' is the platform name for macOS
+    CACHE_FOLDER_PATH = '/Users/riccardoviscio1/Desktop/Projects/F1 Data Analysis/season_2024/f1_cache'
+else:
+    # You may add handling for other platforms if needed
+    raise NotImplementedError("Platform not supported")
+
+print("CACHE_FOLDER_PATH:", CACHE_FOLDER_PATH)
--- F1 Data Analysis/season_2024/utlis.py
+++ F1 Data Analysis/season_2024/utlis.py
@@ -2,6 +2,7 @@ import os
 import fastf1 as ff1
 import plotly
 import plotly.graph_objs as go
+import matplotlib.pyplot as plt
 
 def setup_cache(path):
     if not os.path.exists(path):
@@ -75,3 +76,46 @@ def plot_data_interactive1(df_list, x_col, y_col, labels, circuit_info=None):
 
     fig.show()
 
+
+def plot_race_results(session,session_name, figsize=(12, 5), dpi=300):
+    fig, ax = plt.subplots(figsize=figsize, dpi=dpi)
+    
+    # Plot driver positions
+    for drv in session.drivers:
+        drv_laps = session.laps.pick_driver(drv)
+        abb = drv_laps['Driver'].iloc[0]
+        color = ff1.plotting.driver_color(abb)
+
+        ax.plot(drv_laps['LapNumber'], drv_laps['Position'],
+                label=abb, color=color)
+    
+    # Add starting positions on the left side of the plot
+    for drv in session.drivers:
+        drv_laps = session.laps.pick_driver(drv)
+        abb = drv_laps['Driver'].iloc[0]
+        starting_position = int(drv_laps['Position'].iloc[0])
+        ax.text(0.8, drv_laps['Position'].iloc[0], f"P{starting_position}: {abb} ", color='w', ha='right')
+    
+    # Add final positions and places gained on the right side of the plot
+    for drv in session.drivers:
+        drv_laps = session.laps.pick_driver(drv)
+        abb = drv_laps['Driver'].iloc[0]
+        final_position = drv_laps['Position'].iloc[-1]
+        starting_position = int(drv_laps['Position'].iloc[0])
+        places_gained = int(starting_position - final_position)
+        places_text =  f"({'+' if places_gained > 0 else '' if places_gained == 0 else '-'}{abs(places_gained)})"
+        ax.text(57, final_position, f"P{starting_position}:{abb} {places_text}", color='w', ha='left') 
+
+    # Add annotation for track position on the left side
+    ax.annotate('Track Position', xy=(-0.11, 0.4), xycoords='axes fraction', fontsize=14, color='w', rotation=90)
+
+    ax.set_ylim([20.5, 0.5])
+    ax.set_yticks([])
+    ax.set_xlabel('Lap Number', fontsize=14)
+    ax.set_xlim([1, 57])
+    plt.title(f'2024 {session_name} Grand Prix', font='Arial', fontweight='bold', fontsize=24)
+    plt.tight_layout()
+    plt.show()
+
+# # Example usage
+# plot_race_results(session, 'Bahrain')
